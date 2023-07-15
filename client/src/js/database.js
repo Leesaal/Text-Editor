ce1f619736b4c0a5ee93db7facb66b00120f16c5
Leesa A <leesaali7@gmail.com>
2023-07-15 02:24:22 +0100

    added database todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,54 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // initialise connection with db
+  const db = await openDB("jate", 1);
+
+  //create new transaction, specifying db and data privileges - ReadWrite
+  const tx = db.transaction("jate", "readwrite");
+
+  //open object store
+  const store = tx.objectStore("jate");
+
+  //pass request to store
+  const request = store.put({ id: 1, value: content });
+
+  //wait for completion and confirm
+  const response = await request;
+  console.log("saved to db", response);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // initialise connection with db
+  const db = await openDB("jate", 1);
+
+  //create new transaction, specifying db and data privileges - Readonly
+  const tx = db.transaction("jate", "readonly");
+
+  //open object store
+  const store = tx.objectStore("jate");
+
+  //get all data from store
+  const response = store.getAll();
+
+  //wait for completion and confirm
+  const request = await response;
+  console.log("data has been retrieved", request);
+  return request?.value;
+};
 
 initdb();
